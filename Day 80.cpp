cbd49d2af24580d240479603c0d192c16ce8e6ac
Eshan Sanjay Dhok <95692413+eshanDhok17@users.noreply.github.com>
2023-01-19 13:06:51 +0530

    initial commit

--- Day 80.cpp
+++ Day 80.cpp
@@ -1,4 +1,4 @@
-// Code 1 - Kth Largest Element in an Array
+// Code 1 - Kth Largest Element in an Array (nLogK)
 class Solution {
 public:
     int findKthLargest(vector<int>& A, int k) {
@@ -15,7 +15,54 @@ public:
     }
 };
 
-// Code 2 - Merge K Sorted Lists
-
+// Code 2 - Merge K Sorted Lists (NlogK)
+class Solution {
+public:
+    typedef pair<int, ListNode*> pair;
+    ListNode* mergeKLists(vector<ListNode*>& lists) {
+        if(lists.size() == 0) return nullptr;
+        priority_queue<pair, vector<pair>, greater<pair>> pq;
+        for(int i=0; i<lists.size(); i++) {
+            if(lists[i])
+                pq.push({lists[i]->val, lists[i]});
+        }
+        ListNode dummy(0), *head = &dummy;
+        while(!pq.empty()) {
+            auto top = pq.top(); pq.pop();
+            auto node = top.second;
+            head->next = node;
+            head=head->next;
+            if(node->next)
+                pq.push({node->next->val, node->next});
+        }
+        return dummy.next;
+    }
+};
 
 // Code 3 - Merge K Sorted Arrays
+class Solution {
+    public:
+    vector<int> mergeKArrays(vector<vector<int>> arr, int k) {
+        vector<int> res;
+        int n = arr.size();
+        priority_queue<pair<int, int>> pq;
+        for(int i=0; i<arr.size(); i++) {
+            pq.push({arr[i].back(), i});
+            arr[i].pop_back(); // remove after inserting
+        }
+        
+        while(pq.size() > 0) {
+            auto p = pq.top(); pq.pop();
+            int val = p.first, i = p.second;
+            res.push_back(val);
+            if(arr[i].size()) {
+                // insert next smaller values
+                pq.push({arr[i].back(), i});
+                // remove after inserting
+                arr[i].pop_back();
+            }
+        }
+        reverse(res.begin(), res.end());
+        return res;
+    }
+};
