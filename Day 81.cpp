85e9e51870c6a2bb364e767c9ff74e087d4c17ed
Eshan Sanjay Dhok <95692413+eshanDhok17@users.noreply.github.com>
2023-01-19 16:48:23 +0530

    initial commit

---
+++ Day 81.cpp
@@ -0,0 +1,44 @@
+Code 1 - Minimum Cost to Connect Sticks- O(NlogK)
+
+class Solution {
+public:
+    /**
+     * @param sticks: the length of sticks
+     * @return: Minimum Cost to Connect Sticks
+     */
+    int minimumCost(vector<int> &sticks) {
+        // write your code here
+        int sum = 0;
+        priority_queue<int, vector<int>, greater<>> pq;
+        for(auto &stick : sticks) {
+            pq.push(stick);
+        }
+        while(pq.size() > 1) {
+            int stick1 = pq.top(); pq.pop();
+            int stick2 = pq.top(); pq.pop();
+            sum += stick1 + stick2;
+            pq.push(stick1+stick2);
+        }
+        return sum;
+    }
+};
+
+Code 2 - K Closest Points to Origin - O(NlogK)
+
+class Solution {
+public:
+    vector<vector<int>> kClosest(vector<vector<int>>& points, int k) {
+        vector<vector<int>> res;
+        priority_queue<pair<int, pair<int, int>>> pq;
+        for(auto &point : points) {
+            int x = point[0], y = point[1];
+            pq.push({(pow(x,2) + pow(y, 2)), {x, y}});
+            if(pq.size() > k) pq.pop();
+        }
+        while(!pq.empty()) {
+            auto top = pq.top(); pq.pop();
+            res.push_back({top.second.first, top.second.second});
+        }
+        return res;
+    }
+};
